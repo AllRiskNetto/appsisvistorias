d2e42881417d5bc2af26d508031137ea6fa8f5c2
AllRiskNetto <74000226+AllRiskNetto@users.noreply.github.com>
2020-11-27 09:42:01 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,8 @@ const getUser = async function () {
 
 const api = axios.create({
   //baseURL : 'http://localhost:4200/api/',
-  baseURL: 'http://apphomologacao.sisvistorias.com.br/apivistoria/api/',
-  //'http://appdesenv.sisvistorias.com.br/apivistoria2/api/',
+
+
   // baseURL: 'http://appdesenv.sisvistorias.com.br/apivistoria2/api/',
   // baseURL = 'http://appdesenv.allrisksolucoes.com.br/apivistoria/api/',
 
